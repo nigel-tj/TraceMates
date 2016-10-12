d517221647bc130a1944aca403454e2ada0956be
Nigel <hypej.10@gmail.com>
2016-10-12 02:09:44 +0200

    customise template 2

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    flash[:success ] = "Success Flash Message: Welcome to GentellelaOnRails"
+    #other alternatives are
+    # flash[:warn ] = "Israel don't quite like warnings"
+    #flash[:danger ] = "Naomi let the dog out!"
+  end
+
+end
